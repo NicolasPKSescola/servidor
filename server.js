4d7971c5c59ac48d7aae705994864c5d03ab2d9a
NicolasPKSescola <nicolas.kmick.santos@escola.pr.gov.br>
2026-08-11 17:17:09 -0300

    sla4

--- server.js
+++ server.js
@@ -6,16 +6,38 @@ const PORT = 3004;
 app.use(express.json());
 app.use(cors());
 
-app.post('/cadastro', (req, res)=>{
-    const {usuario, email} = req.body;
+const user = [];
 
-    if (!usuario || !email) {
-        res.status(400).json({
+app.get('/', (req, res)=>{
+    return res.json(user);
+})
+
+app.post('/cadastro', (req, res)=>{
+    const {usuario, email, senha} = req.body;
+
+    if (!usuario || !email || !senha) {
+        return res.status(400).json({
             erro: "Preencha todos os campos"
         });
     }
 
+    if (senha.length < 6) {
+        return res.status(400).json({
+            erro: "A senha deve ter 6 caracteres ou mais"
+        });
+    }
+
+    const emailUsado = user.find((busca) => busca.email === email);
+
+    if (emailUsado) {
+        return res.status(409).json({
+            erro: "Email já está em uso"
+        });
+    }
+
     console.log(`Recebido, aguarde 1 segundo`);
+
+    user.push(usuario, email, senha);
 
     setTimeout(()=>{
         console.log(`Cadastrado o ${usuario}`);
